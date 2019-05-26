a5c49806be117de51d5df574ffd00e1a9d5ab40f
muthuash <51038778+muthuash@users.noreply.github.com>
2019-05-26 08:44:42 +0530

    Create posorneg.java

---
+++ posorneg.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int n;
+	Scanner in=new Scanner(System.in);
+	n=in.nextInt();
+	if(n>0)
+	 System.out.println("Positive");
+	else if(n<0)
+	 System.out.println("Negative");
+	else
+	 System.out.println("Zero");
+	}
+}
